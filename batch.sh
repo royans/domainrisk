3ce45211bb781121b717217643643e84602efd0b
Royans Tharakan <royans@gmail.com>
2024-05-27 15:46:53 -0700

    added a hard timeout

--- batch.sh
+++ batch.sh
@@ -5,14 +5,14 @@ then
 	for host in `cat $1`
 	do
 		echo -n "$host :"; 
-		python3 domainrisk.py $host 2> /dev/null | grep -v Error | tee  /tmp/domain_risk.$host.out | wc -l 
+		timeout 14 python3 domainrisk.py $host 2> /dev/null | grep -v Error | tee  /tmp/domain_risk.$host.out | wc -l 
 	done
 else
 
 for domain in $@ ; 
 do 
 	echo -n "$domain :"; 
-	python3 domainrisk.py $domain 2> /dev/null | grep -v Error | tee  /tmp/domain_risk.$domain.out | wc -l 
+	timeout 14 python3 domainrisk.py $domain 2> /dev/null | grep -v Error | tee  /tmp/domain_risk.$domain.out | wc -l 
 done
 
 fi
