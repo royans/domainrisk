a7cb1a8cdce33a4355d5add52c1242dacdf2d9a8
Royans K Tharakan <royans@gmail.com>
2024-06-30 20:11:19 -0700

    ;sync

--- scripts/crawl.sh
+++ scripts/crawl.sh
@@ -6,14 +6,14 @@ ps -aef | grep updatedb | awk '{print $2}' | xargs kill -9
 load1m=`uptime | cut -d':' -f5 | awk '{print $1}' | cut -d',' -f1`
 load5m=`uptime | cut -d':' -f5 | awk '{print $2}' | cut -d',' -f1`
 
-while awk "BEGIN { exit $load1m > 1.5 }"
+while awk "BEGIN { exit $load1m > 2 }"
 do
 	echo $load1m
 	if awk "BEGIN { exit $load1m > 1.0 }"
 	then
 		if awk "BEGIN { exit $load5m > 1.5 }"
 		then
-      echo "Starting a new thread"
+      echo "==============  Starting a new thread ============"
 			python3 updatedb.py &
 		fi
 	fi
